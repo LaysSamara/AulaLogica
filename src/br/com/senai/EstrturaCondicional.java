ab4d8e5b3723dd0f74a39f609fde115a5a8fa921
LaysSamara <samaracarvalholays@gmail.com>
2021-05-06 15:31:15 -0300

    Classe com exemplo de estrutura condicional, IF - ELSE IF - ELSE

---
+++ src/br/com/senai/EstrturaCondicional.java
@@ -0,0 +1,19 @@
+package br.com.senai;
+
+public class EstrturaCondicional {
+
+	public static void main(String[] args) {
+		int idade = 25;
+				
+		if(idade <12) {
+			System.out.println("Criança");
+		} else if(idade < 18) {
+			System.out.println("Jovem");
+		} else if(idade < 65) {
+			System.out.println("Adulto");
+		} else {
+			System.out.println("Idodo");
+		}
+	}
+
+}
